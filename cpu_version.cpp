97cd3c62c7321e8d8313bad3f37e83034780f21c
Brady Thomas <bethomas@calpoly.edu>
2014-03-07 16:35:17 -0800

    Serial version in C++

--- cpu_version.cpp
+++ cpu_version.cpp
@@ -1,7 +1,7 @@
 //MPI
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 //c++ includes
 /*
@@ -10,44 +10,74 @@
 #include <thrust/device_vector.h>
 #include <thrust/host_vector.h>*/
 #include <cmath>
-
+#include <map>
+#include <string>
+#include <iostream>
 
 //#include <mpi.h>
 
-using std;
+using namespace std;
+
+/* Returns true if string is all alphabetical*/
+bool IsAlpha(char *toCheck) {
+   int count;
+   int len = strlen(toCheck);
+   bool flag = true;
+
+   for (count = 0; count < len && flag; count++) {
+      if (flag = isalpha(toCheck[count])) {
+         toCheck[count] = tolower(toCheck[count]);
+      }
+   }
+   return flag;
+}
+
+map<string, int> countWords(FILE *fp, int fileSize) {
+   map<string, int> words;
+   char *fileData = (char *)malloc(fileSize);
+   string temp;
+   if (!fileData) {
+      perror("Malloc");
+      exit(1);
+   }
+   
+   fread(fileData, sizeof(char),fileSize, fp);
+   char *pch = strtok(fileData, " ,{}\"/=_()-<>`\'!.?:;\n ");
+   while (pch) {
+      if (IsAlpha(pch)) {
+         words[string(pch)]++;
+      }
+      pch = strtok(NULL, " ,{}\"/=_()-<>`\'!.?:;\n");
+   }
+   
+   return words;
+}
+
 
 int main(int argc, char **argv) {
-   
+   map<string, int> words;
 
+   if (argc < 3) {
+      fprintf(stderr, "Usage: %s <infile> <outfile.hist>\n", *argv);
+      exit(1);
+   }
+   
    FILE *fp = fopen(argv[1],"r");
    fseek(fp, 0, SEEK_END);
    int fileSize = ftell(fp);
-   int wordSize = 0;
-   int wordLocation = 0;
    rewind(fp);
 
-   char *fileData = (char *) malloc(fileSize);
-
-   fread(fileData, sizeof(char), fileSize, fp);
-
-   char **wordPtr = (char **) malloc (sizeof(char *) * fileSize/10);
-
-   for (int i = 0; i < fileSize; i++) {
-       *(fileData + i) = *(fileData + i) + 32;
-       
-       /*if((97 <= *(fileData + i) && *(fileData + i) <= 122))
-          if (!wordSize) {
-             *(wordPtr + wordLocation) = fileData + i;
-              wordLocation++;
-          }
-          wordSize++;
-       else if(wordSize) {
-          *(fileData + i) = 0;
-          wordSize = 0
-       }*/
-          
-
-   values = (float*) malloc(sizeof(float) * num_values);
-   fread(values,sizeof(float),num_values,fp);
+   words = countWords(fp, fileSize); 
+   FILE *outfile = fopen(argv[2], "w");
+   if (!outfile) {
+      perror("fopen");
+      exit(1);
+   }
+   
+   for (map<string, int>::iterator it=words.begin(); it!=words.end(); it++){
+      fprintf(outfile, "%s, %d\n", it->first.c_str(), it->second);  
+   }
+   
+   fclose(outfile);
    fclose(fp);
-
+}
