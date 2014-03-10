92fad86d8ddda316a71b8549c7a3c4ffcc7fafd6
zzreardon <zcrdude@aol.com>
2014-03-09 18:05:53 -0700

    Starting the distributed version

--- Makefile
+++ Makefile
@@ -1,9 +1,13 @@
 SERFLAGS=-O2 -o cpu_version
+DISTFLAGS=-O2 -o dist_version
 
 all: serial
 
 serial: cpu_version.cpp
 	nvcc $(SERFLAGS) $^
+
+dist: dist_version.cpp
+	nvcc $(DISTFLAGS) $^ 
 
 clean: 
 	rm -f *.o cpu_version
---
+++ dist_version.cpp
@@ -0,0 +1,111 @@
+//MPI
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//c++ includes
+#include <thrust/device_vector.h>
+#include <thrust/tabulate.h>
+#include <thrust/random.h>
+#include <thrust/transform.h>
+#include <thrust/transform_scan.h>
+#include <thrust/sort.h>
+#include <thrust/reduce.h>
+#include <thrust/binary_search.h>
+#include <thrust/iterator/constant_iterator.h>
+#include <cmath>
+#include <map>
+#include <string>
+#include <iostream>
+#include <vector>
+
+//#include <mpi.h>
+
+using namespace std;
+
+class KeyValue {
+public:
+   const char *key;
+   unsigned value;
+};
+
+/* Returns true if string is all alphabetical*/
+bool IsAlpha(char *toCheck) {
+   int count;
+   int len = strlen(toCheck);
+   bool flag = true;
+
+   for (count = 0; count < len && flag; count++) {
+      if (flag = isalpha(toCheck[count])) {
+         toCheck[count] = tolower(toCheck[count]);
+      }
+   }
+   return flag;
+}
+
+map<string, int> countWords(FILE *fp, int fileSize) {
+   map<string, int> words;
+   char *fileData = (char *)malloc(fileSize);
+   char *last , *secondLast;
+   long t =0;
+   string temp;
+   if (!fileData) {
+      perror("Malloc");
+      exit(1);
+   }
+   
+   fread(fileData, sizeof(char),fileSize, fp);
+   char *pch = strtok(fileData, " ,{}\"/=_()-<>`\'!.?:;\n ");
+   last = pch;
+   while (pch) {
+      if (IsAlpha(pch)) {
+         words[string(pch)]++;
+      }
+      pch = strtok(NULL, " ,{}\"/=_()-<>`\'!.?:;\n");
+   }
+   
+   return words;
+}
+
+
+int main(int argc, char **argv) {
+   map<string, int> words;
+
+   if (argc < 3) {
+      fprintf(stderr, "Usage: %s <infile> <outfile.hist>\n", *argv);
+      exit(1);
+   }
+   
+   FILE *fp = fopen(argv[1],"r");
+   fseek(fp, 0, SEEK_END);
+   int fileSize = ftell(fp);
+   rewind(fp);
+
+   words = countWords(fp, fileSize); 
+   FILE *outfile = fopen(argv[2], "w");
+   if (!outfile) {
+      perror("fopen");
+      exit(1);
+   }
+   :.//
+   thrust::device_vector<const char *> keys(words.size());
+   thrust::device_vector<int> values(words.size());
+   int i= 0;
+  
+   for (map<string, int>::iterator it = words.begin(); it!=words.end(); it++, i++){
+      keys[i] = it->first.c_str();
+      values[i] = it->second;
+   }
+
+   //thrust::sort_by_key(values.begin(), values.end(), keys.begin(), thrust::greater<int>());
+
+   thrust::host_vector<const char *> k = keys;
+   thrust::host_vector<int> v = values;   
+
+   for (int j = 0; j < i; j++) {
+      fprintf(outfile, "%s, %d\n", k[j], v[j]);
+   }
+
+   fclose(outfile);
+   fclose(fp);
+}
